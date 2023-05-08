c345321f0c38e0971b720d34b359158599ffed95
ayaal_fedorov <aiaal8964@mail.ru>
2023-05-08 23:33:45 +0900

    Production

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -11,29 +11,28 @@
         </div>
 
         @foreach($service->clinics as $clinic)
-            <div class="flex p-4 space-x-4 border rounded  justify-between">
-                <a href="{{route('clinic',$clinic->clinic->slug)}}" class="w-[60%] flex space-x-4">
-                    <img src="{{url('images/'.$clinic->clinic->image)}}" class="w-[90px] h-[90px]" alt="">
-                    <div class="flex flex-col space-y-1">
-                        <p class="ml-1"> {{$clinic->clinic->name}} </p>
-                        <div class="flex ml-1 text-xs">
-                            {{$clinic->clinic->type->name}}
-                        </div>
-                        @foreach($clinic->clinic->services as $s)
-                            @if($s->service->id == $service->id)
-                                <p class="ml-1">{{$s->service->name}} от {{$s->price}} руб.</p>
-                                
-                            @endif
-                        @endforeach
+        <div class="lg:flex p-4 lg:space-x-4 border rounded  justify-between">
+            <a href="{{route('clinic',$clinic->clinic->slug)}}" class="w-[60%] flex space-x-4">
+                <img src="{{url('images/'.$clinic->clinic->image)}}" class="w-[90px] h-[90px]" alt="">
+                <div class="flex flex-col space-y-1">
+                    <p class="ml-1 text-sm lg:text-base font-semibold"> {{$clinic->clinic->name}} </p>
+                    <div class="flex ml-1 text-xs">
+                        {{$clinic->clinic->type->name}}
                     </div>
                     
-                </a>
-                
-                <div class="w-[40%] text-sm flex flex-col border-l p-4 justify-center space-y-2">
-                    {{$clinic->clinic->address}}
                 </div>
-
+            </a>
+            <div class="lg:w-[40%] text-sm flex flex-col lg:border-l lg:p-4 py-4 justify-center space-y-2">
+                {{$clinic->address}}
             </div>
+            @foreach($clinic->clinic->services as $s)
+            @if($s->service->id == $service->id)
+                <p class="ml-1">{{$s->service->name}} от {{$s->price}} руб.</p>
+                
+            @endif
+        @endforeach
+        </div>
+
         @endforeach
     </div>
 
